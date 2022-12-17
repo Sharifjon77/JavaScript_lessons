6964aa2532c3b873aee22e45ec481f026fe70d94
sharifjon <112575914+Sharifjon77@users.noreply.github.com>
2022-12-17 22:14:22 +0500

    push

---
+++ DEV QUIZ/lesson_1/javascript.js
@@ -0,0 +1,49 @@
+//console
+
+// console.error("Qanaqadir xatolik");
+// console.warn("Warning");
+// console.log("Console");
+
+//o'zgaruvchilar
+//var, let , const
+
+// if (3 > 2) {
+//   var a = 3;
+//   console.log(a);
+// }
+
+// let a = 3;
+// a = 4;
+// console.log(a);
+
+// const age = 22;
+// age = 21;
+
+// console.log(age);// mumkin emas
+
+// String , number , Boolean , null, undefined, Symbol
+
+//string
+let string = "Hello world";
+console.log(string);
+
+//number
+const num = 15;
+const num1 = 15.5;
+console.log(num, num1);
+
+//Boolean //mantiqiy amal operatori
+const isNum = true;
+const isNum1 = false;
+console.log(isNum, isNum1);
+
+//null
+//o'zgaruvchi mavjud lekin u bo'sh
+const num3 = null;
+
+//undefined
+// o'zgaruvchiga hech qanday qiymat biriktirilmagan
+let num4 = undefined;
+let num5;
+
+console.log(typeof num5); // typeni aniqlab beradi
---
+++ DEV QUIZ/lesson_2/script.js
@@ -0,0 +1,28 @@
+// Konkatenatsiya
+
+/*const name = "Sharifjon";
+const age = 20;
+
+console.log(
+  "Salom mening ismim" + " " + name + " " + "mening yoshim" + " " + age + "da"
+);
+
+console.log(`Salom mening ismim ${name}. Mening yoshim ${age}da`);//ecmascript 6 new version */
+
+//strings
+
+let s = "Salom, men, JavaScriptni, o'rganayapman";
+console.log(s.length);
+console.log(s.toLocaleUpperCase()); //bir xil pastdagi bilan
+console.log(s.toUpperCase()); // bu hammasini bosh harfga o'tkazadi
+console.log(s.substring(0, 5)); // bu kesib oladi
+console.log(s.split(``)); // bu har bir harfni arrayga o'tkazib beradi
+console.log(s.split(" ")); // orasiga probel qo'ysam har bir yozilgan so'zni qirqib beradi
+console.log(s.split(", ")); // ichida belgidan keyingi har bir elementni olib beradi va bu belgi oynadi ko'rinmaydi
+console.log(s.charAt(3)); // gapdagi indeksi bo'yicha 1 dona harfni kesib beradi
+console.log(s.localeCompare(s)); // to'g'ri yoki noto'g'ri ekanligini bildiradi
+
+const str1 = "Hello";
+const str2 = "World";
+console.log(str1.concat(" ", str2)); // bu qo'shib beradi --qavs ichiga probel tashlasa joy tashlaydi yoki biron  belgi yoki harf beriladi--
+console.log(s.endsWith("o'rganayapman", 40)); // bu gapda shu so'z borligini va nechta harf borligi haqida true yoki false habarini beradi (probel hisobga olinmaydi)
